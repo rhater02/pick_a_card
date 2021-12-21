445d643045d40f69e4c1deac11637fe28d6de129
r.hater <r.hater02@gmail.com>
2021-12-21 19:52:42 +0300

    save 4

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -2,5 +2,5 @@
 values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 suits = ['♦', '♥', '♣', '♠']
 
-puts "#{values.sample} of #{suits.sample}"
+puts "#{values.sample}#{suits.sample}"
 puts ":*"
